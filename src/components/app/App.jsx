8f86b7d4b883ed6875b3767593f4824e716291fa
Vlad Belevich <kent35103@gmail.com>
2023-03-17 00:30:38 +0300

    finished

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,7 +7,12 @@ import AccountEdit from '../account/accountEdit';
 import * as actions from '../../services/redux/actions/Actions';
 import ArticleCreate from '../articleCreate';
 import ArticleEdit from '../articleEdit';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 
@@ -27,21 +32,24 @@ function App({ setUserDataFromLocal }) {
     <Router>
       <div>
         <Header />
-        <Route path='/' component={ArticleList} exact />
-        <Route path='/articles/' component={ArticleList} exact />
-        <Route
-          path='/articles/:slug'
-          render={({ match }) => {
-            const { slug } = match.params;
-            return <ArticleFull slug={slug} />;
-          }}
-          exact
-        />
-        <Route path='/sign-in' component={AccountLogin} />
-        <Route path='/sign-up' component={AccountRegister} />
-        <Route path='/profile' component={AccountEdit} />
-        <Route path='/new-article' component={ArticleCreate} exact />
-        <Route path='/articles/:slug/edit' component={ArticleEdit} />
+        <Switch>
+          <Route path='/' component={ArticleList} exact />
+          <Route path='/articles/' component={ArticleList} exact />
+          <Route
+            path='/articles/:slug'
+            render={({ match }) => {
+              const { slug } = match.params;
+              return <ArticleFull slug={slug} />;
+            }}
+            exact
+          />
+          <Route path='/sign-in' component={AccountLogin} />
+          <Route path='/sign-up' component={AccountRegister} />
+          <Route path='/profile' component={AccountEdit} />
+          <Route path='/new-article' component={ArticleCreate} exact />
+          <Route path='/articles/:slug/edit' component={ArticleEdit} />
+          <Redirect to='/' />
+        </Switch>
       </div>
     </Router>
   );
